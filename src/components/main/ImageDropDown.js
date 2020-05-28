84af4beab0b9da76d21a9391ca4627cccf3f2df0
shihongbing <piaolaidebing@126.com>
2020-05-28 10:15:09 +0800

    修改提交

--- src/components/main/ImageDropDown.js
+++ src/components/main/ImageDropDown.js
@@ -11,9 +11,9 @@ class ImageDropDown extends Component{
             <span><Icon name='user' />{this.props.name}</span>
         )
         const options = [
-            { key: 'user', text: '帐号管理11', icon: 'user' },
-            { key: 'settings', text: '系统设置11', icon: 'settings' },
-            { key: 'sign-out', text: '登出11', icon: 'sign out' },
+            { key: 'user', text: '帐号管理123', icon: 'user' },
+            { key: 'settings', text: '系统设置123', icon: 'settings' },
+            { key: 'sign-out', text: '登出123', icon: 'sign out' },
         ]
         return(<Dropdown
             trigger={trigger}
